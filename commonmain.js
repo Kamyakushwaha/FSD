55d0869853649718e8092cb59c4775f629765275
kk <kamya@gmail.com>
2024-10-17 14:14:14 +0530

    repo

---
+++ commonmain.js
@@ -0,0 +1,5 @@
+const check = require("./kk.js");
+console.log("add", check.add(5,3));
+console.log("subtract: ",check.subtract(5,3));
+console.log("PI:",check.PI);
+console.log("Multiply: ",check.multiply(5,3));
---
+++ main.js
@@ -0,0 +1,10 @@
+import { add, subtract,PI} from"./operator.js";
+import * as jadoo from "./operator.js";
+import { multiply } from "./operator.js";
+export{add,subtract,PI,multiply};
+console.log("add:",jadoo.add(5,3));
+console.log("PI: jadoo.PI");
+console.log("multiply:",multiply(5,6));
+
+
+
